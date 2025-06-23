0fe3afe1cca5118e3fe531fa910d6316d9d6c266
liuzx <1091574678@qq.com>
2025-06-23 15:04:47 +0800

    145.二叉树的后序遍历

--- java-template/src/main/java/leetcode/algorithm/binarytree/BinaryTreePostorderTraversal_145.java
+++ java-template/src/main/java/leetcode/algorithm/binarytree/BinaryTreePostorderTraversal_145.java
@@ -7,47 +7,50 @@ import java.util.List;
 import java.util.Stack;
 
 /**
- * 145.二叉树的后序遍历
+ * 145.二叉树的后序遍历（左-右-根）
  */
 public class BinaryTreePostorderTraversal_145 {
 
-    //leetcode submit region begin(Prohibit modification and deletion)
-    /**
-     * Definition for a binary tree node.
-     * public class TreeNode {
-     *     int val;
-     *     TreeNode left;
-     *     TreeNode right;
-     *     TreeNode() {}
-     *     TreeNode(int val) { this.val = val; }
-     *     TreeNode(int val, TreeNode left, TreeNode right) {
-     *         this.val = val;
-     *         this.left = left;
-     *         this.right = right;
-     *     }
-     * }
-     */
     class Solution {
-        public List<Integer> postorderTraversal(TreeNode root) {
+        /**
+         * 递归实现
+         */
+        public List<Integer> postorderTraversal1(TreeNode root) {
             if (root == null) return new ArrayList<>();
             List<Integer> res = new ArrayList<>();
+            res.addAll(postorderTraversal1(root.left));
+            res.addAll(postorderTraversal1(root.right));
+            res.add(root.val);
+            return res;
+        }
+        /**
+         * 栈实现
+         */
+        public List<Integer> postorderTraversal2(TreeNode root) {
+            List<Integer> res = new ArrayList<>();
+            if (root == null) {
+                return res;
+            }
             Stack<TreeNode> stack = new Stack<>();
-            stack.push(root);
-            while (!stack.isEmpty()) {
-                TreeNode node = stack.pop();
-                if (node.left != null) {
-                    stack.push(node.left);
+            TreeNode lastVisited = null; // 标记最后访问的节点
+            while (!stack.isEmpty() || root != null) {
+                while (root != null) {
+                    stack.push(root);
+                    root = root.left;
                 }
-                if (node.right != null) {
-                    stack.push(node.right);
+                TreeNode peekNode = stack.peek();
+                // 如果右子节点为空或者已经被访问过
+                if (peekNode.right == null || peekNode.right == lastVisited) {
+                    res.add(peekNode.val);
+                    lastVisited = stack.pop();
+                } else {
+                    root = peekNode.right;
                 }
             }
+
             return res;
         }
     }
-    //leetcode submit region end(Prohibit modification and deletion)
-
-    
     public static void main(String[] args) {
         Solution solution = new BinaryTreePostorderTraversal_145().new Solution();
         // put your test code here
